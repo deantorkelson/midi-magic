a1d577941b9def573174b88f7e2b323c1a95bf83
Dean Torkelson <dean.torkelson@silvercar.com>
2022-07-25 16:53:47 -0500

    improve structure

--- main.py
+++ main.py
@@ -3,13 +3,59 @@ from midiutil import MIDIFile
 from helpers.helpers import find_instrument_program
 
 
+class MidiMagicFile:
+    def __init__(self, song_dir, file_name, midi):
+        self.song_dir: str = song_dir
+        self.file_name: str = file_name
+        self.midi: MIDIFile = midi
+        self.line_index: int = 0
+
+    # processes the MidiMagic text file into midi on the given track
+    def process_to_track(self, track):
+        instrument: str = self.file_name.split('.')[0]
+        # lookup instrument program number
+        self.midi.addTrackName(track, 0, instrument)
+        self.midi.addProgramChange(track, track, 0, find_instrument_program(instrument))
+        file = open(f"songs/{self.song_dir}/{self.file_name}", "r")
+        lines: list[str] = file.readlines()
+        done_reading_file = False
+        # process each measure
+        while not done_reading_file:
+            measure = self.read_in_next_measure(lines)
+            if len(measure) == 0:
+                done_reading_file = True
+                continue
+            self.measure_to_midi(measure)
+
+
+    def measure_to_midi(self, measure):
+        # read in metadata (clef, dynamics, etc.)
+        # read the measure from left to right
+        # if a note is encountered
+        #   add it to the midi file
+        #   at the end of reading that column, increase `time` based on the largest note encountered
+        return
+
+    def read_in_next_measure(self, lines) -> list[str]:
+        measure = []
+        num_lines = len(lines)
+        new_measure_found = False
+        while self.line_index < num_lines and not new_measure_found:
+            if not lines[self.line_index].strip()[0].isdigit():
+                # not in a new measure, add the line to the measure we're looking at and keep going
+                measure.append(lines[self.line_index])
+                self.line_index += 1
+            else:
+                # new measure found, stop reading and parse the previous measure
+                new_measure_found = True
+        return measure
+
 class MidiMagic:
     def __init__(self, song_dir):
         self.song_dir = song_dir
 
     def create_midi(self):
         song_files = os.listdir(f"songs/{self.song_dir}")
-        num_channels = len(song_files) - 1
         print(song_files)
         midi = MIDIFile(1)
         track = 0
@@ -17,47 +63,11 @@ class MidiMagic:
             time = 0
             if file_name == 'meta.txt':
                 continue
-            instrument = file_name.split('.')[0]
-            # lookup instrument program number
-            midi.addTrackName(track, 0, instrument)
-            midi.addProgramChange(track, track, 0, find_instrument_program(instrument))
-            file = open(f"songs/{self.song_dir}/{file_name}", "r")
-            lines = file.readlines()
-            num_lines = len(lines)
-            line_index = 0
-            done_reading_file = False
-            while not done_reading_file:
-                measure = []
+            MidiMagicFile(self.song_dir, file_name, midi).process_to_track(track)
             track += 1
         with open("generated_midi/test.mid", "wb") as output_file:
             midi.writeFile(output_file)
         print('done')
-
-
-        # read in the entire file with readlines
-        # iterate through and grab the current measure
-        #   do this by going from start (init to 0) up to but not including the next line that starts with a number
-        # once all the lines are collected
-        # read in metadata (clef, dynamics, etc.)
-        # read the measure from left to right
-        # if a note is encountered
-        #   add it to the midi file
-        #   at the end of reading that column, increase `time` based on the largest note encountered
-
-    def read_next_measure(self, lines) -> list[list[str]]:
-        # self.line_index doesn't make sense - should make a new MidiFile class or something
-        # that handles reading in each of the files
-        measure = []
-        new_measure_found = False
-        while line_index < num_lines and not new_measure_found:
-            if not lines[line_index].strip()[0].isdigit():
-                # not in a new measure, add the line to the measure we're looking at and keep going
-                measure.append(lines[line_index])
-                line_index += 1
-            else:
-                # new measure found, stop reading and parse the previous measure
-                new_measure_found = True
-        return ''
 
 
 magic = MidiMagic('the-lick')
